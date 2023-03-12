fa76f98f4a2671f0365eda8f64856030e8719bbd
katkaaa <98588418+katkaaa@users.noreply.github.com>
2023-03-12 18:37:00 +0100

    Add main.py, extensions/fun.py, requirements.txt and .gitignore

---
+++ extensions/fun.py
@@ -0,0 +1,31 @@
+import hikari
+import lightbulb
+import aiohttp
+import json
+
+fun = lightbulb.Plugin("fun")
+
+@fun.command
+@lightbulb.command("kitty", "Awww 🥰 ")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def kitty(ctx : lightbulb.Context):
+    embed = hikari.Embed(title="A kitty pic for you! <3").set_image("https://cataas.com/cat").set_footer(f"Powered by Cataas")
+    await ctx.respond(embed=embed)
+
+@fun.command
+@lightbulb.command("meme", "The big funny")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def meme(ctx : lightbulb.Context):
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://meme-api.com/gimme/1") as meme:
+            jsonvalue = await meme.json()
+            image = jsonvalue["memes"][0]["url"]
+            subreddit = jsonvalue["memes"][0]["subreddit"]
+            author = jsonvalue["memes"][0]["author"]
+            title = jsonvalue["memes"][0]["title"]
+            embed = hikari.Embed(title = f"{title}").set_image(image).set_footer(text = f"r/{subreddit} - u/{author} | Powered by meme-api")
+            await ctx.respond(embed = embed)
+
+
+def load(bot):
+    bot.add_plugin(fun)
---
+++ main.py
@@ -0,0 +1,40 @@
+import hikari
+import lightbulb
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("token")
+intents = hikari.Intents.ALL # Edit this based on what intents you have set in your developer portal
+bot = lightbulb.BotApp(token = token, prefix="s!", intents=intents, help_class=None)
+
+@bot.command
+@lightbulb.command("help", "The epic help message")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def help_message(ctx : lightbulb.Context):
+    embed = hikari.Embed(title = "Here to help!").add_field(name="Fun", value="`meme`\n`kitty`").add_field(name="Misc", value="`ping`\n`apis`")
+    await ctx.respond(embed)
+
+
+@bot.command
+@lightbulb.command("ping", "Checks the bot's latency")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping_command(ctx: lightbulb.Context):
+      embed = hikari.Embed(title = "Pong! :ping_pong:", description = f"{bot.heartbeat_latency * 1000:.0f}ms")
+      await ctx.respond(embed=embed)
+
+
+@bot.command
+@lightbulb.command("apis", "I wonder what apis does this bot use!")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping_command(ctx: lightbulb.Context):
+      embed = hikari.Embed(title="**API List**", description="Cat as a service - *https://cataas.com/* \nMeme API - *https://github.com/D3vd/Meme_Api*")
+      await ctx.respond(embed=embed)
+
+
+for filename in os.listdir("./extensions"):
+    if filename.endswith(".py"):
+        bot.load_extensions(f"extensions.{filename[:-3]}")
+
+bot.run()
